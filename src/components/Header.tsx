b8c2291176cc2c9cff57a2d15c3fdd923cc4decc
abraselrn-seccional <abraselrn@gmail.com>
2026-09-03 15:24:45 -0300

    Update 'Next Event' link path to September

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 
   const navLinks = [
     { label: 'Início', path: '/' },
-    { label: 'Próximo Evento', path: '/evento/agosto'},
+    { label: 'Próximo Evento', path: '/evento/setembro'},
     { label: 'Conheça a Abrasel', path: '/sobre' },
     { label: 'Seja um Parceiro', path: '/parceiros' },
   ];
